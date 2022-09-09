8dc2ff52bd16d3009407ccaa60b33beb94025bd0
Huy Nam TRAN <huynam1012@gmail.com>
2022-09-09 15:06:21 +0200

    ajoute component ajouter nature

---
+++ src/app/nature-mission/add-nature/add-nature.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-nature',
+  templateUrl: './add-nature.component.html',
+  styleUrls: ['./add-nature.component.css']
+})
+export class AddNatureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/nature-mission/nature-mission.module.ts
+++ src/app/nature-mission/nature-mission.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { CreationNatureComponent } from './creation-nature/creation-nature.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ModifyNatureComponent } from './modify-nature/modify-nature.component';
+import { AddNatureComponent } from './add-nature/add-nature.component';
 
 
 
 @NgModule({
   declarations: [
     CreationNatureComponent,
-    ModifyNatureComponent
+    ModifyNatureComponent,
+    AddNatureComponent
   ],
   imports: [
     CommonModule,
