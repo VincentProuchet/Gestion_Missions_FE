b2ad33d00e4b6a74aadee248ea088908617770c6
Vincent Prouchet <prouchetvincent@hotmail.com>
2022-09-09 15:45:50 +0200

    Interface City
    correction import
     Creation-natureComponent

--- src/app/model/mission.ts
+++ src/app/model/mission.ts
@@ -1,5 +1,25 @@
+import { Nature } from "../nature-mission/Natures";
+import { CityService } from "../service/city.service";
+import { City } from "./city";
+import { Collaborator } from "./collaborator";
+import { Expense } from "./expense";
+import { Status } from "./status";
+import { Transport } from "./transport";
+
 /**
  * Mission
  */
 export interface Mission {
+  id: number;
+  bonus: number;
+  status: Status,
+  transport: Transport;
+  start: Date;
+  end: Date;
+  startCity: City;
+  arrivalCity: City;
+  nature: Nature;
+  collaborator: Collaborator;
+  expenses: Expense[];
+
 }
