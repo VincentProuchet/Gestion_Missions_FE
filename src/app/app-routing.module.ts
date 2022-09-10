1d8b392ac4da8b987bbf39cb673a7de6f3ee78c8
Vincent Prouchet <prouchetvincent@hotmail.com>
2022-09-10 14:22:46 +0200

    Corrigé la route vers
     la gestion des notes de frais

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AllExpensesComponent } from './expenses/all-expenses/all-expenses.component';
 import { ChargesComponent } from './expenses/charges/charges.component';
+import { ExpensesModule } from './expenses/expenses.module';
 import { LoginComponent } from './login/login.component';
 import { AllMissionsComponent } from './missions/all-missions/all-missions.component';
 import { CreateMissionComponent } from './missions/create-mission/create-mission.component';
@@ -37,7 +38,7 @@ const routes: Routes = [
   {
     // page de saisie des frais
     path: 'saisieFrais',
-    component: AllMissionsComponent,
+    component: AllExpensesComponent,
   },
   {
     // page de gestion des natures de missions
