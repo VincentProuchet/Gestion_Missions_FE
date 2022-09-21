64cc13747f3a1b34efe5ac9e635f8c329c40974a
DorianBoel <dorian.boel@free.fr>
2022-09-21 12:24:49 +0200

    add form builder & validators to login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  incorrectCredentials: boolean = false;
+
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = formBuilder.group({
+      usernameControl: ['', [Validators.required]],
+      passwordControl: ['', [Validators.required]],
+      rememberMeCheckbox: [true]
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) {
+      return;
+    }
+  }
+
+  inputIsInvalid(inputName: string) {
+    let isTouched: boolean = this.loginForm.controls[inputName].invalid && (this.loginForm.controls[inputName].dirty || this.loginForm.controls[inputName].touched);
+    let required: boolean = this.loginForm.controls[inputName].errors?.['required'];
+    return isTouched && required;
   }
 
 }
